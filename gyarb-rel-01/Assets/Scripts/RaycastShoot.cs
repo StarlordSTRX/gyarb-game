f41deb061efedf626d8d556e974baa22300c05ad
Lenore <snabbspeed@gmail.com>
2016-10-04 13:57:53 +0200

    Started Working on the gun bransh
    +gun (invisible)
    + raytraceShoot script
    + general prep

---
+++ gyarb-rel-01/Assets/Scripts/RaycastShoot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastShoot : MonoBehaviour {
+
+
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hiForce = 100f;
+    public Transform gunEnd;
+
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+
+    //https://youtu.be/AGd16aspnPA?t=536
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
