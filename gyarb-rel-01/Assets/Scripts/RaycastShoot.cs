3a0f191cd33f6ac27b68837fabf96afebdbb976e
Lenore <snabbspeed@gmail.com>
2016-10-10 16:20:19 +0200

    Fixed model, added scripts
    + Added raycast shoot script
    + Added raycast view script (for debug)
    * Fixed arm/gun model

--- gyarb-rel-01/Assets/Scripts/RaycastShoot.cs
+++ gyarb-rel-01/Assets/Scripts/RaycastShoot.cs
@@ -1,29 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RaycastShoot : MonoBehaviour {
+public class RaycastShoot : MonoBehaviour
+{
 
 
     public int gunDamage = 1;
     public float fireRate = .25f;
     public float weaponRange = 50f;
-    public float hiForce = 100f;
+    public float hitForce = 100f;
     public Transform gunEnd;
 
 
     private Camera fpsCam;
-    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f); //decleared here to optimize memory preformance
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
 
     //https://youtu.be/AGd16aspnPA?t=536
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
 }
