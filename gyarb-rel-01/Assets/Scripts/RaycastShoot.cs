7da842e06aef38cd10b2a9b888edcd9fc4e98a6e
Lenore <snabbspeed@gmail.com>
2016-10-18 11:32:15 +0200

    Added Shootable Entity & Crosshair
    + Shootable Entity
    + Crosshair

--- gyarb-rel-01/Assets/Scripts/RaycastShoot.cs
+++ gyarb-rel-01/Assets/Scripts/RaycastShoot.cs
@@ -8,7 +8,7 @@ public class RaycastShoot : MonoBehaviour
     public int gunDamage = 1;
     public float fireRate = .25f;
     public float weaponRange = 50f;
-    public float hitForce = 100f;
+    public float hitForce = 600f;
     public Transform gunEnd;
 
 
@@ -46,6 +46,16 @@ public class RaycastShoot : MonoBehaviour
             if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
             {
                 laserLine.SetPosition(1, hit.point);
+
+                //Send damage
+                ShootableEntity health = hit.collider.GetComponent<ShootableEntity>();
+                if (health != null) {
+                    health.Damage(gunDamage);
+                }
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
             }
             else
             {
---
+++ gyarb-rel-01/Assets/Scripts/ShootableEntity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootableEntity : MonoBehaviour {
+
+    public int currentHealth = 3;
+
+	
+	// Update is called once per frame
+	public void Damage(int damageAmount) {
+
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
