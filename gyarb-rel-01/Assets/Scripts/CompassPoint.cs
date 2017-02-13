2ffb97989825f1a8ccfba143b25e6900c6bdeb5d
Lenore <snabbspeed@gmail.com>
2017-02-13 15:31:57 +0100

    Started Compass
    +Added Compass
    -It's not working
    TODO: fix auto Reload

---
+++ gyarb-rel-01/Assets/Scripts/CompassPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompassPoint : MonoBehaviour {
+
+    public float angleBetween = 0.0F;
+    public Transform target;
+
+    void Start() {
+       
+    }
+
+    void Update() {
+
+        Vector3 targetDir = target.position - transform.position;
+        angleBetween = Vector3.Angle(transform.forward, targetDir);
+
+        gameObject.transform.Rotate(targetDir, angleBetween);
+	}
+}
