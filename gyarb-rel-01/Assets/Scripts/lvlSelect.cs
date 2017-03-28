85b772e15908c43829822186ed4da16d97f5d819
Lenore <snabbspeed@gmail.com>
2017-03-28 13:31:23 +0200

    lvlSelect-prepared
    + Started working on Level select
    * It doesn't work

---
+++ gyarb-rel-01/Assets/Scripts/lvlSelect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class lvlSelect : MonoBehaviour {
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void LevelFour()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
